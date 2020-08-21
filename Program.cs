d84f5802a8f90718a1a42e5d9eea984a2b752a18
davidbrucetn <davidbrucetn@gmail.com>
2020-08-21 10:14:45 -0500

    Phase 4

--- Program.cs
+++ Program.cs
@@ -11,40 +11,42 @@ namespace EnthusiasticMoose
             Console.WriteLine();
 
             //Let the moose speak by calling the function
-            MooseSays();
+            // passing literal string as parameter
+            MooseSays("H I, I'M  E N T H U S I A S T I C !");
+            MooseSays("I really am enthusiastic");
         }
 
-        static void MooseSays()
+        static void MooseSays(string message)
         {
-            // @ sign lets you add blank lines
-            Console.WriteLine(@"
-                                       _.--^^^--,
-                                    .'           `\
-  .-^^^^^^-.                      .'               |
+            // $ sign for string interpolation and @ symbol for blank lines
+            Console.WriteLine($@"
+                                      _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
  /          '.                   /            .-._/
 |             `.                |             |
-\              \          .-._ |          _   \
- `^^'-.         \_.-.     \   `          ( \__/
-       |             )     '=.       .,   \
-      /             (         \     /  \  /
-    /`               `\        |   /    `'
-    '..-`\        _.-. `\ _.__/   .=.
-         |  _    / \  '.-`    `-.'  /
-         \_/ |  |   './ _     _  \.'
-              '-'    | /       \ |
-                    |  .-. .-.  |
-                    \ / o| |o \ /
-                    |   / \   |    H I, I'M  E N T H U S I A S T I C !
-                    / `^`   `^` \
-                    /             \
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
                     | '._.'         \
                     |  /             |
-                    \ |             |
-                    ||    _    _   /
-                    /|\  (_\  /_) /
-                    \ \'._  ` '_.'
-                    `^^` `^^^`
-            ");
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+        ");
         }
     }
 }
