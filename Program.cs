05f00100ad97820d665aaa6be1fd5f17dffd41ab
davidbrucetn <davidbrucetn@gmail.com>
2020-08-21 10:35:38 -0500

    Phase 5

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 namespace EnthusiasticMoose
 {
+    //Class is container for methods
     class Program
     {
         static void Main(string[] args)
@@ -14,6 +15,38 @@ namespace EnthusiasticMoose
             // passing literal string as parameter
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("I really am enthusiastic");
+
+            // As a question, stores boolean return in isTrue
+            bool isTrue = MooseAsks("Is Canada real?");
+
+            // Prints boolean return
+            Console.WriteLine(isTrue);
+        }
+
+        // Make the moose ask questions
+        // MooseAsks method returns a boolean value bool,
+        // and accepts a string question
+        static bool MooseAsks(string question)
+        {
+            Console.Write($"{question} (Y/N): ");
+            string answer = Console.ReadLine().ToLower();
+
+            // While answer is not y or n, keep asking question
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write($"{question} (Y/N): ");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            // Received a y or n after while loop, return boolean
+            if (answer == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         static void MooseSays(string message)
